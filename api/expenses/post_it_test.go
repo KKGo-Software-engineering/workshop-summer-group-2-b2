8c1f6570d4dd9b8a50a1ab767951ede633278e80
Earth Varis <varis.a@siamraj.com>
2024-05-19 14:29:13 +0700

    fix test id not match

--- api/expenses/post_it_test.go
+++ api/expenses/post_it_test.go
@@ -6,44 +6,40 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
 	"github.com/KKGo-Software-engineering/workshop-summer/api/config"
 	"github.com/KKGo-Software-engineering/workshop-summer/api/model"
 	"github.com/KKGo-Software-engineering/workshop-summer/migration"
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
-	"net/http"
-	"net/http/httptest"
-	"testing"
-	"time"
 )
 
-func getTestDatabaseFromConfig() (*sql.DB, error) {
-	cfg := config.Parse("DOCKER")
-	sql, err := sql.Open("postgres", cfg.PostgresURI())
-	if err != nil {
-		return nil, err
-	}
-	return sql, nil
-}
+// func getTestDatabaseFromConfig() (*sql.DB, error) {
+// 	cfg := config.Parse("DOCKER")
+// 	sql, err := sql.Open("postgres", cfg.PostgresURI())
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return sql, nil
+// }
 
 func TestCreateExpensesIT(t *testing.T) {
 	t.Run("create Expense successfully when feature toggle is enable", func(t *testing.T) {
 		transactions := model.Transaction{
 			Date:            time.Date(2024, 5, 19, 20, 10, 0, 0, time.UTC),
-			Amount:          150.75,
+			Amount:          999.75,
 			Category:        "Groceries",
 			TransactionType: "Expense",
 			Note:            "Weekly grocery shopping",
 			ImageURL:        "http://example.com/receipt.jpg",
 			SpenderId:       5,
 		}
-		sql, err := getTestDatabaseFromConfig()
-		if err != nil {
-			t.Error(err)
-		}
-		migration.ApplyMigrations(sql)
-		defer migration.RollbackMigrations(sql)
+		sql := newDatabase(t)
 
 		h := New(config.FeatureFlag{EnableCreateSpender: true}, sql)
 		e := echo.New()
@@ -51,7 +47,7 @@ func TestCreateExpensesIT(t *testing.T) {
 
 		e.POST("/expenses", h.Create)
 
-		jsonTransaction, err := json.Marshal(transactions)
+		jsonTransaction, _ := json.Marshal(transactions)
 		req := httptest.NewRequest(http.MethodPost, "/expenses", bytes.NewReader(jsonTransaction))
 		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 		rec := httptest.NewRecorder()
@@ -59,6 +55,32 @@ func TestCreateExpensesIT(t *testing.T) {
 		e.ServeHTTP(rec, req)
 
 		assert.Equal(t, http.StatusCreated, rec.Code)
-		assert.JSONEq(t, `{"amount":150.75, "category":"Groceries", "date":"2024-05-19T20:10:00Z", "id":1, "image_url":"http://example.com/receipt.jpg", "note":"Weekly grocery shopping", "spender_id":5, "transaction_type":"Expense"}`, rec.Body.String())
+		var got model.Transaction
+		err := json.Unmarshal(rec.Body.Bytes(), &got)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, transactions.Date, got.Date)
+		assert.Equal(t, transactions.Amount, got.Amount)
+		assert.Equal(t, transactions.Category, got.Category)
+		assert.Equal(t, transactions.TransactionType, got.TransactionType)
+		assert.Equal(t, transactions.Note, got.Note)
+		assert.Equal(t, transactions.ImageURL, got.ImageURL)
+		assert.Equal(t, transactions.SpenderId, got.SpenderId)
+		// assert.JSONEq(t, `{"amount":999.75, "category":"Groceries", "date":"2024-05-19T20:10:00Z", "id":1, "image_url":"http://example.com/receipt.jpg", "note":"Weekly grocery shopping", "spender_id":5, "transaction_type":"Expense"}`, rec.Body.String())
 	})
+}
+
+func newDatabase(t *testing.T) *sql.DB {
+	cfg := config.Parse("DOCKER")
+	sql, err := sql.Open("postgres", cfg.PostgresURI())
+	if err != nil {
+		t.Fatal(err)
+	}
+	migration.ApplyMigrations(sql)
+	t.Cleanup(func() {
+		sql.Query("DELETE FROM public.transaction WHERE amout = 999.75 ;")
+		sql.Close()
+	})
+	return sql
 }
